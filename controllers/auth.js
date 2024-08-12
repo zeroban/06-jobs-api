7d106a135d677376f90245834f6fc6e474d09722
zeroban <theemptyspamfolder@gmail.com>
2024-08-12 10:03:20 -0400

    week9 assignment 06-JOBS-API

---
+++ controllers/auth.js
@@ -0,0 +1,25 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+
+const { BadRequestError } = require('../errors')
+const bcrypt = require('bcryptjs')
+
+
+
+
+const register = async (req, res) => {
+
+    const user = await User.create({ ...req.body })
+    res.status(StatusCodes.CREATED).json({ user })
+}
+
+const login = async (req, res) => {
+    res.send('login user')
+}
+
+
+
+module.exports = {
+    register,
+    login,
+}
---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],
+        minlength: 3,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide a email'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide a valid email'
+        ],
+        unique: true
+
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 6
+    },
+})
+
+userSchema.pre('save', async function () {
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+
+})
+
+module.exports = mongoose.model('user', userSchema)
