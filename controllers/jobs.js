9204a23a4e793ef483294e193f1a2f739adc2ae4
zeroban <theemptyspamfolder@gmail.com>
2024-08-13 19:09:59 -0400

    updated job models and controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,28 +1,66 @@
-const Job = require('../models/Job')
+const Tires = require('../models/Job')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
 
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs')
+    const tires = await Tires.find({ createdBy: req.user.userId }).sort('createdAt')
+    res.status(StatusCodes.OK).json({ tires, count: tires.length })
 }
 
 const getJob = async (req, res) => {
-    res.send('get job')
+    // searches for the userID and tireID to make sure they match
+    const { user: { userId }, params: { id: tireID }, } = req
+
+    // will locate the tireID that was created by the user searching
+    const tires = await Tires.findOne({ _id: tireID, createdBy: userId })
+
+    if (!tires) {
+        throw new NotFoundError(`No tire found with ${tireID}`)
+    }
+    res.status(StatusCodes.OK).json({ tires })
 }
 
 const createJob = async (req, res) => {
     req.body.createdBy = req.user.userId
-    const job = await Job.create(req.body)
-    res.status(StatusCodes.CREATED).json({ job })
+    const tires = await Tires.create(req.body)
+    res.status(StatusCodes.CREATED).json({ tires })
 }
 
 const updateJob = async (req, res) => {
-    res.send('update job')
+    // searches for the userID and tireID to make sure they match
+    const {
+        body: { brand, size, location, price, quantity },
+        user: { userId },
+        params: { id: tireID },
+    } = req
+
+    // will check for blank fields
+    if (brand === "" || size === "" || location === "" || price === "" || quantity === "") {
+        throw new BadRequestError(`Be sure that all fields are filled in. Can not be null/blank`)
+    }
+
+    const tires = await Tires.findByIdAndUpdate({ _id: tireID, createdBy: userId }, req.body, { new: true, runValidators: true })
+
+    if (!tires) {
+        throw new NotFoundError(`No tire found with ID: ${tireID}`)
+    }
+    res.status(StatusCodes.OK).json({ tires })
+
 }
 
 const deleteJob = async (req, res) => {
-    res.send('delete job')
+    // searches for the userID and tireID to make sure they match
+    const { user: { userId }, params: { id: tireID }, } = req
+
+    const tires = await Tires.findByIdAndRemove({
+        _id: tireID,
+        createdBy: userId
+    })
+    if (!tires) {
+        throw new NotFoundError(`No tire found with ID: ${tireID}`)
+    }
+    res.status(StatusCodes.OK).send()
 }
 
 
--- models/Job.js
+++ models/Job.js
@@ -1,20 +1,36 @@
 const mongoose = require('mongoose')
 
-const JobSchema = new mongoose.Schema({
-    company: {
+// worked on building out my own schema like that assignment suggested
+const tireSchema = new mongoose.Schema({
+    brand: {
         type: String,
-        require: [true, 'Please provide company name'],
+        require: [true, 'Please provide the brand of the tire'],
         maxlenght: 50
     },
-    position: {
+    size: {
         type: String,
-        require: [true, 'Please provide position'],
-        maxlenght: 100
+        require: [true, 'please enter in the size of the tire'],
+        maxlenght: 15
     },
-    status: {
+    location: {
         type: String,
-        enum: ['interview', 'declined', 'pending'],
-        default: 'pending'
+        require: [true, 'Please provide a location of where the tires are located'],
+        enum: ['Mebane', 'Greensboro'],
+        default: 'Mebane'
+    },
+    price: {
+        type: Number,
+        require: [true, 'please enter in the price of each tire'],
+        min: 0 // ensures the price is not negative 
+    },
+    warranty: {
+        type: String,
+        maxlenght: 50
+    },
+    quantity: {
+        type: Number,
+        require: [true, 'please enter the quantity of tires you have'],
+        min: 0 // ensures they can not enter in a negative number 
     },
     createdBy: {
         type: mongoose.Types.ObjectId,
@@ -23,4 +39,5 @@ const JobSchema = new mongoose.Schema({
     }
 }, { timestamps: true })
 
-module.exports = mongoose.model('Job', JobSchema)
+
+module.exports = mongoose.model('tires', tireSchema)
