4804ce98ac9cfc9cde7cbd768fe28ae74fa972ab
zeroban <theemptyspamfolder@gmail.com>
2024-08-17 12:38:42 -0400

    Updated middleware error handler and added additional packages for security

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,41 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+
+
+  let customError = {
+    // set defaults
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong try again later'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  // leaving commented code out for possibly future use if needed
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+
+  // searching for validation error when registering user
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors).map((item) => item.message).join(', ')
+    customError.statusCode = 400 // error code should be 400
+  }
+
+  // searching for is user already has a registerd account based off email address
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
+    customError.statusCode = 400 // error code should be 400
+  }
+
+  // searching for cast error when searching for single tire ID
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id: ${err.value}`
+    customError.statusCode = 404 // error code should be 404
+  }
+
+  // leaving commented code out for possibly future use if needed
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({ msg: customError.msg })
+
 }
 
 module.exports = errorHandlerMiddleware
